30a3196aaa530b284d4b720dfbb55a06cfdcf233
Jorge Luis <delangeldelangeljorgeluis@gmail.com>
2022-12-28 20:28:01 -0600

    exported core

---
+++ lib/core/app/app.dart
@@ -0,0 +1,6 @@
+export 'package:messages/core/app/acount.dart';
+export 'package:messages/core/app/auth.dart';
+export 'package:messages/core/app/chat.dart';
+export 'package:messages/core/app/contact.dart';
+export 'package:messages/core/app/group.dart';
+export 'package:messages/core/app/history.dart';
